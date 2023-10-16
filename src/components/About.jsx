864e1ebf2de285e48bec327825fc69306930ee6a
Rohit Tiwari <110346246+tiwaritech18@users.noreply.github.com>
2023-10-16 22:55:15 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,8 @@ function AboutSection() {
           <div className="text-side">
             <h1>About me</h1>
             <h4>A dedicated Front-end Developer <br /> based in Uttarakhand, India 📍</h4>
-            <p>As a developer I love to create websites using HTML, CSS, JavaScript and React. I have a degree in computer science and some projects on my GitHub profile. I am looking for a software developer job where I can learn and grow. Please contact me via email or LinkedIn if you are interested. Thank you. 😊<p>
+            <p>As a developer I love to create websites using HTML, CSS, JavaScript and React. I have a degree in computer science and some projects on my GitHub profile. I am looking for a software developer job where I can learn and grow. Please contact me via email or LinkedIn if you are interested. Thank you.
+<p>
           </div>
         </div>
       </div>
